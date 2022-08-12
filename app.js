80b8c3ac24fb9306cd5cea92431fe0a0a68c4f09
Mikhail Sosnin <originid1488@ya.ru>
2022-08-12 14:13:27 +0300

    fix: установлен rate limit and helmet

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+/* eslint-disable no-console */
 require('dotenv').config();
-
+const rateLimit = require('express-rate-limit');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
 const { PORT } = process.env;
+
+// DDoS protector
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter);
+
+// Сonfiguring headers for protection
+const helmet = require('helmet');
+
+app.use(helmet());
 
 // Временное решение авторизации
 app.use((req, res, next) => {
