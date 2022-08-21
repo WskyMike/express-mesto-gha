687e1619d7b2bc7c9712f5780ae58cb5fbe3b51f
Mikhail Sosnin <originid1488@ya.ru>
2022-08-21 19:48:58 +0300

    fix: correct routers

--- routes/users.js
+++ routes/users.js
@@ -9,9 +9,9 @@ const {
 const { userAboutValidation, avatarValidation, idValidation } = require('../middlewares/validate');
 
 userRouter.get('/users', getUsers);
-userRouter.get('/users/:_id/', idValidation, getUserById);
 userRouter.get('/users/me', idValidation, getCurrentUser);
 userRouter.patch('/users/me', userAboutValidation, updateUser);
+userRouter.get('/users/:_id/', idValidation, getUserById);
 userRouter.patch('/users/me/avatar', avatarValidation, updateAvatar);
 
 module.exports = { userRouter };
